15c4758017e03509dd2236abec0219ba35c279f7
soadar <60164893+soadar@users.noreply.github.com>
2023-12-20 01:51:09 -0300

    Update RedditVideo.js

--- RedditVideo.js
+++ RedditVideo.js
@@ -1,87 +1,45 @@
 // ==UserScript==
-// fuente / autor 1N07
-// Solo edite la url de descarga, ya que viddit y ripsave no me funcionan
+// @name         Reddit Video 1N07 Mod By Soadar2
+// @autor        1N07
 // @edit: 		 Soadar
-// @name         Reddit Video Downloader
-// @namespace    1N07
-// @version      0.6
+// @namespace    Soadar
+// @version      0.7
 // @description  Lets you download reddit videos with sound using RedditSave
 // @author       1N07
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js
 // @match        https://www.reddit.com/*
-// @exclude      https://www.reddit.com/message/compose/*
-// @grant       GM_registerMenuCommand
-// @grant       GM_unregisterMenuCommand
-// @grant       GM_getValue
-// @grant       GM_setValue
-// @noframes
+// @exclude      https://www.reddit.com/message/*
+// @icon         https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    $(function(){
-        AddButtons();
-        setInterval(AddButtons, 200);
-    });
+    AddButtons();
+    setInterval(AddButtons, 1000);
 
     function AddButtons() {
-        //=== post page ===//
-        if(window.location.href.includes("/comments/"))
+        let vids = $("shreddit-player");
+        if(vids.length > 0 && !vids.find("source").prop("src").includes("external-preview.redd.it"))
         {
-            let vid = $("div[data-test-id='post-content'] video");
-            if(vid.length > 0 && !vid.find("source").prop("src").includes("external-preview.redd.it"))
-            {
-                let bar = $("div[data-test-id=post-content] > div:last");
-                if(bar.length > 0 && bar.find(".downloadVid").length == 0) {
-                    let saveButt = bar.find("button .icon-save").parent().parent();
-                    saveButt.prop("style", "float: left;");
-                    saveButt.after(`<div class="outerForDLB"></div>`);
-                    bar.find(".outerForDLB").append(saveButt.clone().addClass("downloadVid"));
-                    let dlButt = bar.find(".downloadVid");
-                    dlButt.find("i.icon").removeClass("icon-save").addClass("icon-downvote");
-                    dlButt.find("span:last").html('Download');
-                    bar.find(".outerForDLB").prop("style", "float: right;");
-                    //console.log("dlb: " + dlButt.length);
-
-                    dlButt.click(function(e){
-                        e.preventDefault();
-                        let dlUrl = window.location.href.split("#")[0].split("?")[0];
-						let urlCompleta = 'https://redditsave.com/info?url=' + dlUrl;
-                        window.open(urlCompleta, "_blank");
-                    });
+            let player = document.querySelectorAll('[post-type="video"')
+            player.forEach((video) => {
+                const menu = video.shadowRoot.querySelector('[name="comments-action-button"]').parentNode.parentNode;
+                if (menu.querySelector(".downloadVid") === null) {
+                    video.style.marginBottom = "60px";
+                    const urlCompleta = 'https://redditsave.com/info?url=https://www.reddit.com' + video.permalink;
+                    const btn = document.createElement("a");
+                    btn.innerHTML = "Download";
+                    btn.setAttribute("style", "float: right;");
+                    btn.setAttribute("width", "50px");
+                    btn.style.padding= "5px";
+                    btn.style.cursor = "pointer"
+                    btn.style.backgroundColor ="#88D966";
+                    btn.style.color ="black";
+                    btn.classList.add("downloadVid");
+                    btn.onclick = () => window.open(urlCompleta, "_blank");;
+                    menu.lastElementChild.parentNode.append(btn);
                 }
-            }
+            });
         }
-
-        //=== browse page ===//
-        let targets = $("div.scrollerItem div > video");
-        targets.each(function(){
-            if($(this).find("source").length > 0 && !$(this).find("source").prop("src").includes("external-preview.redd.it"))
-            {
-                let bar = $(this).parent().parent().parent().parent().nextAll("div:last");
-                if(bar.find(".icon-save").length == 0)
-                    bar = bar.prev().parent().parent().nextAll("div:last");
-                if(bar.find(".downloadVid").length == 0)
-                {
-                    let saveButt = bar.find("button .icon-save").parent().parent();
-                    if(saveButt == null) alert("saveButt null!");
-                    saveButt.prop("style", "float: left;");
-                    saveButt.after(`<div class="outerForDLB"></div>`);
-                    bar.find(".outerForDLB").append(saveButt.clone().addClass("downloadVid"));
-                    let dlButt = bar.find(".downloadVid");
-                    dlButt.find("i.icon").removeClass("icon-save").addClass("icon-downvote");
-                    dlButt.find("span:last").html('Download');
-                    bar.find(".outerForDLB").prop("style", "float: right;");
-
-                    dlButt.click(function(e){
-                        e.preventDefault();
-                        let dlUrl = $(this).closest('div[data-click-id="background"]').find("a[data-click-id=body]:first").prop("href").split("#")[0].split("?")[0];
-                        let urlCompleta = 'https://redditsave.com/info?url=' + dlUrl;
-                        window.open(urlCompleta, "_blank");
-                    });
-                }
-            }
-        });
     }
 })();
